c19e747f38f442c5e0079e83c44082ee9f2a0443
varunudata <varunudata@gmail.com>
2025-12-03 15:21:36 +0530

    checking pagination

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -61,36 +61,31 @@ const getAllCategories = async (req, res) => {
 const getCategoryById = async (req, res) => {
   try {
     const id = Number(req.params.id);
-
     const page = Number(req.query.page) || 1;
     const limit = 6;
     const skip = (page - 1) * limit;
-
     const sort = req.query.sort === "oldest" ? "asc" : "desc";
-    // default = latest
-
     const category = await prisma.category.findUnique({
       where: { id },
     });
-
     if (!category) {
       return res.status(404).json({
         success: false,
         message: "Category not found",
       });
     }
-
+    console.log("PARAMS:", req.params);
+    console.log("QUERY:", req.query);
+    console.log("URL:", req.originalUrl);
     const posts = await prisma.post.findMany({
       where: { categoryId: id },
       skip,
       take: limit,
       orderBy: { createdAt: sort },
     });
-
     const totalPosts = await prisma.post.count({
       where: { categoryId: id },
     });
-
     return res.status(200).json({
       success: true,
       data: {
